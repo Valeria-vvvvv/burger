c31807ffaacf3e56fb4a73a57ebaba56e1c760bc
Valeria Novoselova <v_lelik_v260702@mail.ru>
2025-11-26 12:38:00 +0300

    убрали импорт тестового файла

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { AppRoutes } from "./components/routes/AppRoutes";
 import { AppProvider } from "./components/providers/AppProvider";
 import { Notifications } from "./components/Notifications/Notifications";
-import "./utils/seedData"; // Импортируем для доступа к window.seedData()
 import "./App.css";
 
 export const App = () => {
